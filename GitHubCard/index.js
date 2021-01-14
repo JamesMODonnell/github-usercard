2ff7b3e4f92d07cf80d4b10f53a72f05798bd3f1
JamesMODonnell <james.odonnell401@gmail.com>
2021-01-14 14:07:46 -0500

    initial commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,11 @@
+
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
+    https://api.github.com/users/JamesMODonnell
 */
-
+const BASE_URL = 'http://api.github.com/users/JamesMODonnell'
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
